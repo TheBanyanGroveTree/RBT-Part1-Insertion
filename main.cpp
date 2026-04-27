82b648a5920016e9350edb7d00142b92d4f100e6
Aahana Sapra <sapraaahana@gmail.com>
2026-04-27 00:34:43 -0700

    Define rotation functions

--- main.cpp
+++ main.cpp
@@ -16,12 +16,22 @@ using namespace std;
 void stringUpper(string& input);
 
 // add()
+
+void leftRotate(Node* x, Node* NIL, Node*& root);
+void rightRotate(Node* x, Node* NIL, Node*& root);
+
 // read()
 // print()
 
+
 int main() {
-  // Define RBT root Node
-  Node* root = nullptr;
+  // Initialize NIL (sentinel Node for leaves)
+  Node* NIL = new Node(0);
+  NIL->setIsRed(false);
+  NIL->setLeft(NIL);
+  NIL->setRight(NIL);
+
+  Node* root = NIL; // Initialize RBT root Node
 
   string userCommand = ""; // Declare str for user input
 
@@ -52,9 +62,67 @@ int main() {
   return 0;
 }
 
+
 // Convert string to uppercase
 void stringUpper(string& input) {
   for (int i = 0; i < input.size(); i++) {
       input[i] = toupper(input[i]);
     }
 }
+
+
+// Implement left rotation (promote root successor)
+void leftRotate(Node* x, Node* NIL, Node*& root) {
+  Node* y = x->getRight(); // Node to be promoted
+
+  // 1. HANDOVER: detach subtree (NEW x right subtree = OLD y left subtree)
+  x->setRight() = y->getLeft();
+  if (y->getLeft() != NIL) {
+    y->getLeft()->setParent() = x;
+  }
+
+  // 2. PARENT LINK: connect y to tree
+  y->setParent() = x->getParent(); // NEW y parent = OLD x parent
+  
+  // Connect NEW x parent to y
+  if (x->getParent() == nullptr) { // x = ROOT
+    root = y;
+  } else if (x == x->getParent->getLeft()) { // x = LEFT child
+    x->getParent->setLeft() = y;
+  } else { // x = RIGHT child
+    x->getParent->setRight = y;
+  }
+
+  // 3. PIVOT: finalize x-y PARENT-CHILD link
+  y->setLeft() = x; 
+  x->setParent = y;
+}
+
+
+// Implement right rotation (promote root predecessor)
+void rightRotate(Node* x, Node* NIL, Node*& root) {
+  Node* y = x->getLeft(); // Node to be promoted
+
+  // 1. HANDOVER: detach subtree (NEW x left subtree = OLD y right subtree)
+  x->setLeft() = y->getRight();
+  if (y->getRight != NIL) {
+    y->getRight->setParent() = x;
+  }
+
+  // 2. PARENT LINK: connect y to tree
+  y->setParent = x->getParent();
+
+  // Connect NEW x parent to y
+  if (x->getParent() == nullptr) { // x = ROOT
+    root = y;
+  } else if (x == x->getParent->getLeft()) { // x = LEFT child
+    x->getParent->setLeft() = y;
+  } else { // x = RIGHT child
+    x->getParent->setRight = y;
+  }
+
+  // 3. PIVOT: finalize x-y PARENT-CHILD link
+  y->setRight() = x;
+  x->setParent() = y;
+}
+
